ebeb959b9274c5c6e0bbd3b7b3f125e170f9595b
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2014-01-18 16:21:49 +0000

    some command processing, unreliable, running out of ram

--- sketch/fermagent.ino
+++ sketch/fermagent.ino
@@ -10,7 +10,6 @@
 #include <avr/power.h>
 #include <avr/sleep.h>
 
-
 int sleepStatus = 0;             // variable to store a request for sleep
 int count = 0;                   // counter
 
@@ -69,7 +68,6 @@ void notice(String msg);
 void  debug(String msg);
 void   info(String msg);
 void  error(String msg);
-String strDate(DateTime);
 void   outputSensor(String type, String id, String outstr);
 
 /* Read the internal voltage */ 
@@ -118,31 +116,31 @@ SdVolume volume;
 SdFile root;
 void cardInfo(void){
   // print the type of card
-  debug("\nCard type: ");
+  debug("Card type: "));
   switch(SD.type()) {
     case SD_CARD_TYPE_SD1:
-      debug("SD1");
+      debug("SD1"));
       break;
     case SD_CARD_TYPE_SD2:
-      debug("SD2");
+      debug("SD2"));
       break;
     case SD_CARD_TYPE_SDHC:
-      debug("SDHC");
+      debug("SDHC"));
       break;
     default:
-      debug("Unknown");
+      debug("Unknown"));
   }
 
   // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
   if (!volume.init(SD)) {
-    debug("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    debug("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card"));
     return;
   }
 
 
   // print the type and size of the first FAT-type volume
   uint32_t volumesize;
-  debug("\nVolume type is FAT");
+  debug("Volume type is FAT"));
   debug(String(volume.fatType(), DEC));
   
   volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
@@ -150,9 +148,9 @@ void cardInfo(void){
   volumesize *= 512;                            // SD card blocks are always 512 bytes
   volumesize /= 1024;
   volumesize /= 1024;
-  debug("Volume size (Mbytes): " + String(volumesize));
+  debug(String("Volume size (Mbytes): ") + String(volumesize));
 
-  //debug("Files found on the card (name, date and size in bytes): ");
+  //debug("Files found on the card (name, date and size in bytes): "));
   //root.openRoot(volume);
   //root.ls(LS_R | LS_DATE | LS_SIZE);
 }
@@ -162,29 +160,39 @@ void cardInfo(void){
 #define BT_SERIAL  1
 #define CHAN_CNT   2
 #define CHAN_SZ    64 
-char *chan_buffs[CHAN_CNT];
+char  chan_buffs[CHAN_CNT][CHAN_SZ];
 int   chan_curr[CHAN_CNT];
 
 void setupChans(void){
   for(int i = 0 ; i < CHAN_CNT ; i++){
-     chan_buffs[i] = (char *) malloc(CHAN_SZ * sizeof(char));
      chan_curr[i]  = 0;
   }
 }
 
+bool debugMsgs = false;
 void cmd_debug(int argc, char** argv){
-  debug("Debug Command: " + String(argv[0]));
-  for(int i = 1; i <= argc; i++){
-    info("Arg[" + String(i,DEC) + "]: " + String(argv[i]));
+  if(argc != 1){
+    if (debugMsgs){
+      info(String("debug on"));
+    } else {
+      info(String("debug off"));
+    };
+  } else {
+    if (String(argv[1]) == String("on")){
+      debugMsgs = true;
+      info(String("debug on"));
+    } else {
+      debugMsgs = false;
+      info(String("debug off"));
+    };
   }
 };
 
-void cmd_viewlog(int argc, char** argv){
-  debug("ViewLog Command: " + String(argv[0]));
-  for(int i = 1; i <= argc; i++){
-    info("Arg[" + String(i,DEC) + "]: " + String(argv[i]));
-  }
+void cmd_view(int argc, char** argv){
 };
+
+void cmd_poll(int argc, char** argv);
+
 
 typedef void (*cmd_func)(int argc, char** argv);
 typedef struct cmd_hash_item_s {
@@ -193,62 +201,70 @@ typedef struct cmd_hash_item_s {
 } cmd_hash_item;
 
 cmd_hash_item cmd_hash[] = {
-  {"debug",   cmd_debug},
-  {"viewlog", cmd_viewlog},
+  {String("debug"),   cmd_debug},
+  {String("view"), cmd_view},
+  {String("poll"), cmd_poll},
   {NULL,NULL}
 };
 
 void run_cmd(int argc, char** argv){
   String cmd = String(argv[0]);
+
+  debug(String("Command: ") + cmd);
+  for(int i = 1; i <= argc; i++){
+    debug(String("Arg[") + String(i,DEC) + "]: " + String(argv[i]));
+  }
+
   for(int i = 0; cmd_hash[i].name != NULL; i++){
     if(cmd_hash[i].name == cmd){
       (cmd_hash[i].f)(argc, argv); 
       return;
     }
   }
-  error("Unknown Command: " + cmd);
+
+  error(String("Unknown Command: ") + cmd);
 }
 
 #define ARGS_MAX  8
 void dispatch_cmd(int chan){
-  debug(String(chan_buffs[chan]));
-
+  char* buff = chan_buffs[chan];
   int argc = 0;
-  int len = strlen(chan_buffs[chan]);
   char* argv[ARGS_MAX];
   memset(argv,0,ARGS_MAX * sizeof(char*));
-  argv[argc] = chan_buffs[chan];
+  argv[0] = buff;
 
-  for(int i = 0; i < len; i++){
-    if((chan_buffs[chan])[i] == (char) NULL){
-      break;
-    } else if ((chan_buffs[chan])[i] == ' '){
-      (chan_buffs[chan])[i] = (char) NULL;
-      i++;
+  for(; *buff != (char) NULL; buff++){
+    if (*buff == ' '){
+      *buff = (char) NULL;
+      buff++;
       argc++;
-      argv[argc] = (chan_buffs[chan]) + i;
+      argv[argc] = buff;
     }
   }
 
-   run_cmd(argc,argv); 
+  run_cmd(argc,argv); 
 }
 
-void dispatch_error(int can, String err){
-  error("Error on chan[" + String(can, DEC) + "]: " + err);
+void dispatch_error(int chan, String err){
+  error(String("Error on chan[") + String(chan, DEC) + String("]: ") + err);
 }
 
 void dispatchByte(int chan, char c){
+  int pos = chan_curr[chan];
+  
   if (c == '\r'){
-    (chan_buffs[chan])[chan_curr[chan]] = 0;
+    chan_buffs[chan][pos] = (char) 0;
     dispatch_cmd(chan);
     chan_curr[chan] = 0;
+    chan_buffs[chan][0] = 0;
   } else {
     if (chan_curr[chan] >= CHAN_SZ){
-      dispatch_error(chan,"Input buffer overflow");
+      dispatch_error(chan,String("Input too big"));
+      chan_buffs[chan][0] = 0;
       chan_curr[chan] = 0;
     } else {
-      (chan_buffs[chan])[chan_curr[chan]] = c;
-      chan_curr[chan]++;
+      chan_buffs[chan][pos] = c;
+      chan_curr[chan] = pos + 1;
     }
   }
 }
@@ -261,9 +277,9 @@ void setupSDCardReader(void){
   info("card initialized.");
   // we'll use the initialization code from the utility libraries
   // since we're just testing if the card is working!
-
 }
 
+void printDate(int,DateTime);
 RTC_DS1307 RTC; // define the Real Time Clock object
 void setupRTC(void){
   // connect to RTC
@@ -273,8 +289,11 @@ void setupRTC(void){
 
   if (! RTC.isrunning()) {
     DateTime builddate = DateTime(__DATE__, __TIME__);
-    info("RTC is NOT running!");
-    info("Attempt to set date to " + strDate(builddate));
+    info("RTC not running!");
+    info("Set date to ") ;
+    printDate(USB_SERIAL, builddate);
+    Serial.println();
+
     RTC.adjust(builddate);
   }
 }
@@ -290,8 +309,8 @@ void setup(void) {
   Wire.begin();
   Serial.begin(9600);
 
-  setupChans();
   setupRTC();
+  setupChans();
   setupBluetooth();
   setupSDCardReader();
   setupRemoteControl();
@@ -311,7 +330,7 @@ void scanDSSensors(void){
   OneWire ds(ONEWIREPIN);
 
   while (ds.search(addr)) {
-    const String unknown = "Unknown";
+    const String unknown = String("Unknown");
     String type = unknown;
     String id = "";
     String msg = "";
@@ -333,7 +352,7 @@ void scanDSSensors(void){
         break;
       }
       default: {
-        notice("-- Unknown device type");
+        notice("Unknown device type");
         continue;
       }
     }
@@ -354,13 +373,13 @@ void scanDSSensors(void){
     ds.select(addr);
     ds.write(0xBE);         // Read Scratchpad
 
-    msg = "P=" + String(present, HEX);
+    msg = String("P=") + String(present, HEX);
     msg += " D=";
     for ( i = 0; i < 9; i++) {           // we need 9 bytes
       data[i] = ds.read();
       msg += String(data[i], HEX);
     }
-    msg += (" CRC=");
+    msg += String(" CRC=");
     msg += String(OneWire::crc8( data, 8), HEX);
 
     LowByte = data[0];
@@ -375,7 +394,6 @@ void scanDSSensors(void){
 
     Whole = Tc_100 / 100;  // separate off the whole and fractional portions
     Fract = Tc_100 % 100;
-
 
     msg += " T=";
     if (SignBit) // If its negative
@@ -407,44 +425,61 @@ void scanSensors(void) {
   scanDSSensors();
 }
 
-String strDate(DateTime dt){
-  String res = "";
-  res += dt.year();
-  res += '/';
-  res += dt.month();
-  res += '/';
-  res += dt.day();
-  res += ' ';
-  res += dt.hour();
-  res += ':';
-  res += dt.minute();
-  res += ':';
-  res += dt.second();
+void cmd_poll(int argc, char** argv){
+  scanSensors();
+};
 
-  return res;
+void printDate(int chan,DateTime dt){
+  if(chan == USB_SERIAL){
+    Serial.print(dt.year());
+    Serial.print('/');
+    Serial.print(dt.month());
+    Serial.print('/');
+    Serial.print(dt.day());
+    Serial.print(' ');
+    Serial.print(dt.hour());
+    Serial.print(':');
+    Serial.print(dt.minute());
+    Serial.print(':');
+    Serial.print(dt.second());
+  } else if (chan == BT_SERIAL) {
+    BlueSerial.print(dt.year());
+    BlueSerial.print('/');
+    BlueSerial.print(dt.month());
+    BlueSerial.print('/');
+    BlueSerial.print(dt.day());
+    BlueSerial.print(' ');
+    BlueSerial.print(dt.hour());
+    BlueSerial.print(':');
+    BlueSerial.print(dt.minute());
+    BlueSerial.print(':');
+    BlueSerial.print(dt.second());
+  }
 }
 
 
 void loop(void) {
   int idle_count = 0;
   bool idle = true;
-
-  scanSensors();
+  int incoming;
+  //scanSensors();
 
   // Wait for command-line input
   while(Serial.available() > 0)
   {
-    dispatchByte(USB_SERIAL, (char) Serial.read());
+    incoming = Serial.read();
+    dispatchByte(USB_SERIAL, (char)incoming);
     idle_count = 0;
     idle = false;
   }
 
-  while(BlueSerial.available() > 0)
-  {
-    dispatchByte(BT_SERIAL, (char) BlueSerial.read());
-    idle_count = 0;
-    idle = false;
-  }
+//  while(BlueSerial.available() > 0)
+//  {
+//    incoming = BlueSerial.read();
+//    dispatchByte(BT_SERIAL,incoming) ;
+//    idle_count = 0;
+//    idle = false;
+//  }
 
 //  mySwitch.sendTriState("00000FFF0F0F");
 //  delay(1000);
@@ -455,7 +490,7 @@ void loop(void) {
 
   if(idle){
     idle_count++;
-    delay(1000);
+    //delay(1000);
   };
 
   //if(idle && idle_count > 10){
@@ -464,31 +499,34 @@ void loop(void) {
 }
 
 // How we output sensor values
-bool debugMsgs = true;
-void outputSensor(String type, String id, String outstr){
+// These log to the serial port
+void logbase(String prefix, String lvl, String msg){
   DateTime now = RTC.now();
-  Serial.println("++ " + strDate(now) + " " + type + "[" + id + "] " + outstr);
-  if( debugMsgs){
-    BlueSerial.println("++ " + strDate(now) + " " + type + "[" + id + "] " + outstr);
-  }
+  Serial.print(prefix);
+  printDate(USB_SERIAL,now);
+  Serial.print(" ");
+  Serial.print(lvl);
+  Serial.print(": ");
+  Serial.flush();
+  Serial.println(msg);
+  Serial.flush();
+//  if( debugMsgs){
+//   BlueSerial.println(String("# ") + strDate(now) + String(" ") + lvl + String(": ") + msg);
+//  BlueSerial.flush();
+//}
 }
 
-// These log to the serial port
-void logbase(String lvl, String msg){
-  DateTime now = RTC.now();
-  Serial.println("-- " + strDate(now) + " " + lvl + ": " + msg);
-  if( debugMsgs){
-    BlueSerial.println("-- " + strDate(now) + " " + lvl + ": " + msg);
-  }
+void outputSensor(String type, String id, String outstr){
+  logbase("!" ,"+", type + String("[") + id + String("] ") + outstr);
 }
 
 void  debug(String msg){
   if( debugMsgs){
-    logbase("DEBUG" , msg);
+    logbase(String("!"),String("D") , msg);
   }
 }
 
-void notice(String msg){logbase("NOTICE", msg);}
-void   info(String msg){logbase("INFO"  , msg);}
-void  error(String msg){logbase("ERROR" , msg);}
+void notice(String msg){logbase(String("#"),String("N"), msg);}
+void   info(String msg){logbase(String("#"),String("I"), msg);}
+void  error(String msg){logbase(String("#"),String("E"), msg);}
 
